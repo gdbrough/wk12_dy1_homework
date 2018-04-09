c2fe4dbe9ce14698dc973e993b2568c8eb805e60
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-09 16:33:35 +0100

    adds cat data via JS and removes HTML

--- public/app.js
+++ public/app.js
@@ -6,23 +6,23 @@ var createCatArticle = function(){
 };
 
 // 2. Create first child and its text
-var createCatName = function(){
+var createCatName = function(name){
   let catName = document.createElement("li");
-  catName.innerText = "Name: Catty McCatface";
+  catName.innerText = name;
   return catName;
 }
 
 // 3. Create the 2nd child and its text
-var createCatFood = function(){
+var createCatFood = function(food){
   let catFood = document.createElement("li");
-  catFood.innerText = "Favourite food: Pot Noodle";
+  catFood.innerText = food;
   return catFood;
 };
 
 // 3. Create the 3rd child and its text
-var createCatPic = function(){
+var createCatPic = function(picture){
   let catPic = document.createElement("li");
-  catPic.innerHTML = "<img width=\"500\" src=\"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg\">";
+  catPic.innerHTML = picture;
   return catPic;
 };
 
@@ -40,19 +40,34 @@ var appendElements = function(catUL, catName, catFood, catPic){
 
 
 
-var addCat = function(catName, catfood, catPic) {
+var addCat = function(catName, catFood, catPic) {
   var catArticle = createCatArticle();
-  var nameOfCat = createCatName(catName);
-  var favFoodOfCat = createCatFood(catFood);
-  var picOfCat = createCatPic(catPic);
+  var nameOfCat = createCatName("Name: " + catName);
+  var favFoodOfCat = createCatFood("Favourite food: " + catFood);
+  var picOfCat = createCatPic("<img width=\"500\" src=\"" + catPic + "\">");
   appendElements(catArticle, nameOfCat, favFoodOfCat, picOfCat);
 };
 
-var app = function(){
-  catName = "Someone Wise";
-  catFood = "Assumption is the mother of all fuck ups.";
-  catPic = "<img width=\"500\" src=\"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg\">";
-  addCat(catName, catFood, catPic);
+// var app = function(){
+//   catName = "Name: Catty McCatface";
+//   catFood = "Favourite food: Pot Noodle";
+//   catPic = "<img width=\"500\" src=\"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg\">";
+//   addCat(catName, catFood, catPic);
+// };
+
+var newapp = function(){
+  catNames = ["Boba","Barnaby","Max","Catty McCatface"];
+  catFoods = ["Sock fluff","Tuna","Whiskas Temptations","Pot Noodle"];
+  catPics = [
+    "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg",
+    "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg",
+    "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg",
+    "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"
+  ];
+
+  for (index = 0; index < catNames.length; index++){
+    addCat(catNames[index], catFoods[index], catPics[index]);
+  };
 };
 
-window.onload = app;
+window.onload = newapp;
