38911dd7ea5819484c1f72c372d72271b51146d4
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-09 16:08:46 +0100

    adds functions to cat add

--- public/app.js
+++ public/app.js
@@ -1,31 +1,61 @@
-const app = function(){
+// const app = function(){
 
 // 1. create parent container and its class
-let catUL = document.createElement("ul");
-catUL.classList.add("cat");
+var createCatArticle = function(){
+  let catUL = document.createElement("ul");
+  catUL.classList.add("cat");
+  return catUL;
+};
 
 // 2. Create first child and its text
-let catName = document.createElement("li");
-catName.innerText = "Name: Catty McCatface";
+var createCatName = function(){
+  let catName = document.createElement("li");
+  catName.innerText = "Name: Catty McCatface";
+  return catName;
+}
 
 // 3. Create the 2nd child and its text
-let catFood = document.createElement("li");
-catFood.innerText = "Favourite food: Pot Noodle";
-
+var createCatFood = function(){
+  let catFood = document.createElement("li");
+  catFood.innerText = "Favourite food: Pot Noodle";
+  return catFood;
+};
 
 // 3. Create the 3rd child and its text
-let catPic = document.createElement("li");
-catPic.innerHTML = "<img width=\"500\" src=\"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg\">";
+var createCatPic = function(){
+  let catPic = document.createElement("li");
+  catPic.innerHTML = "<img width=\"500\" src=\"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg\">";
+  return catPic;
+};
 
+var appendElements = function(catUL, catName, catFood, catPic){
 // 4. Append the li to the ul
-catUL.appendChild(catName);
-catUL.appendChild(catFood);
-catUL.appendChild(catPic);
+  catUL.appendChild(catName);
+  catUL.appendChild(catFood);
+  catUL.appendChild(catPic);
 
 // 5. Append the ul to the list of cats
-let cats = document.querySelector("#cats");
-cats.appendChild(catUL)
+  let cats = document.querySelector("#cats");
+  cats.appendChild(catUL)
 
+};
+
+
+
+var addCat = function(catName, catfood, catPic) {
+  var catArticle = createCatArticle();
+  var nameOfCat = createCatName(catName);
+  var favFoodOfCat = createCatFood(catFood);
+  var picOfCat = createCatPic(catPic);
+  // var cite = createCite(author);
+  appendElements(catArticle, nameOfCat, favFoodOfCat, picOfCat);
+};
+
+var app = function(){
+  catName = "Someone Wise";
+  catFood = "Assumption is the mother of all fuck ups.";
+  catPic = "<img width=\"500\" src=\"https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg\">";
+  addCat(catName, catFood, catPic);
 };
 
 window.onload = app;
